e9354e46c6ada85ccd5d3b8e4bea5af10a91809b
FelipeCGibaja <150200634+sargon494@users.noreply.github.com>
2025-10-15 21:14:00 +0200

    class help

--- src/mostrarAyuda.java
+++ src/mostrarAyuda.java
@@ -4,15 +4,15 @@ public class mostrarAyuda {
 
         System.out.println("""
                 Comandos disponibles en MiniShell:\n
-                ls - Listar archivos y directorios en el directorio actual.\n
-                cd - Cambiar el directorio actual.\n
-                pwd - Mostrar el directorio actual.\n
-                mkdir - Crear un nuevo directorio.\n
-                rm - Eliminar un archivo o directorio.\n
-                mv - Mover o renombrar archivos y directorios.\n
-                cp - Copiar archivos.\n
-                exit - Salir del MiniShell.\n
-                help - Mostrar esta ayuda.\n
+                ls - Listar archivos y directorios en el directorio actual.
+                cd - Cambiar el directorio actual.
+                pwd - Mostrar el directorio actual.
+                mkdir - Crear un nuevo directorio.
+                rm - Eliminar un archivo o directorio.
+                mv - Mover o renombrar archivos y directorios.
+                cp - Copiar archivos.
+                exit - Salir del MiniShell.
+                help - Mostrar esta ayuda.
                 
                 """);
 
